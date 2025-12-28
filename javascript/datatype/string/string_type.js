de1bfe3b9eedda60822f4451a7ac7fe2f7aacd8f
ljsoung <ljsoung0923@sunmoon.ac.kr>
2025-12-28 14:16:22 +0900

    string 타입 다루기

---
+++ javascript/datatype/string/string_type.js
@@ -0,0 +1,4 @@
+let string1 = "Hello, World!";
+let string2 = "Hello, World!";
+console.log(string1);
+console.log(string2);
---
+++ javascript/datatype/string/string_type_escape2.js
@@ -0,0 +1,2 @@
+let string = "이스케이프 문자를 이용해서\n줄 바꿈하고 싶어요";
+console.log(string);
