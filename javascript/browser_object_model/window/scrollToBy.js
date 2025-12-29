a5b5d331422fb4592262296c0d45bf9a32ee0d44
ljsoung <ljsoung0923@sunmoon.ac.kr>
2025-12-29 13:24:23 +0900

    브라우저 객체 모델 사용

---
+++ javascript/browser_object_model/window/scrollToBy.js
@@ -0,0 +1,3 @@
+// 웹 브라우저의 스크롤 이동
+window.scrollTo(100, 200);
+window.scrollBy(100, 200);
---
+++ javascript/browser_object_model/window/scrollToByBehavior.js
@@ -0,0 +1,2 @@
+window.scrollTo({top:4000, behavior:'smooth'});
+window.scrollBy({top:600, behavior:'smooth'});
